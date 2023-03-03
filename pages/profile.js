5de5d211fe6271eb82a3d4a276e3ae86493ec1ae
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-03-02 22:32:18 -0600

    profile info is blank wrote a ticket

---
+++ pages/profile.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import UserForm from '../components/forms/UserForm';
+
+export default function UserProfile() {
+  const { user, signOut } = useAuth();
+  return (
+    <>
+      <UserForm user={user} />
+      <div style={{ marginTop: '35px' }}>
+        <Image src={user.image} alt="userURL" width="100px" height="100px" />
+        <h1>Name: {user.name}</h1>
+        <h2>Bio: {user.bio}</h2>
+        <Button variant="danger" onClick={signOut}> Sign Out</Button>
+      </div>
+    </>
+  );
+}
