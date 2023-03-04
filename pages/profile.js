414a3f10037e3d9332a4ff056452456dfbbd2793
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-03-04 10:49:37 -0600

    profile still not working but getting closer

--- pages/profile.js
+++ pages/profile.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useAuth } from '../utils/context/authContext';
+import { getSingleUser } from '../api/userData';
 
-// I need user.image/name/bio to display but they are not showing up when I click on Profile
+// I need profileDetails.image/name/bio to display but they are not showing up when I click on Profile
 // These are the data that are input in UserForm
 export default function UserProfile() {
   const { user, signOut } = useAuth();
+  const [profileDetails, setProfileDetails] = useState({});
+
+  useEffect(() => {
+    getSingleUser(user).then((profileData) => {
+      setProfileDetails(profileData);
+      console.warn(user.uid);
+    });
+  }, [user]);
   return (
     <>
       <div style={{ marginTop: '35px' }}>
-        <img src={user.image} alt="userURL" width="100px" height="100px" />
-        <h1>Name: {user.name}</h1>
-        <h2>Bio: {user.bio}</h2>
+        <img src={profileDetails.image} alt="yourmom" width="100px" height="100px" />
+        {console.warn('profileDetails:', profileDetails)}
+        <h1>Name: {profileDetails.name}</h1>
+        <h2>Bio: {profileDetails.bio}</h2>
         <Button variant="danger" onClick={signOut}> Sign Out</Button>
       </div>
     </>
