928a42e047f55aa4020dce73ebb72e41a216ed5a
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-03-10 02:07:26 -0600

    really making cards look crisp tonight

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import Link from 'next/link';
 
 function UserCard({ userObj }) {
   return (
-    <Card style={{ width: '18rem', margin: '10px' }}>
-      <Card.Img variant="top" src={userObj.image} alt={userObj.name} style={{ height: '400px' }} />
-      <Card.Body>
-        <Card.Title>{userObj.name}</Card.Title>
-        <Link href={`/users/${userObj.firebaseKey}`} passHref>
-          <Button variant="primary" className="m-2">view</Button>
-        </Link>
-      </Card.Body>
+    <Card style={{ width: '18rem', height: '5rem', margin: '10px' }}>
+      <Row>
+        <Col xs={4}>
+          <Card.Img
+            variant="top"
+            src={userObj.image}
+            alt={userObj.name}
+            style={{ height: '100px', width: '100px' }}
+          />
+        </Col>
+        <Col xs={8} className="d-flex align-items-center justify-content-center">
+          <Link href={`/users/${userObj.firebaseKey}`} passHref>
+            <Card.Link className="hoverText">{userObj.name}</Card.Link>
+          </Link>
+        </Col>
+      </Row>
     </Card>
   );
 }
