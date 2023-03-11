7175ae38f95f7fa2f56785305a0094033b886920
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-03-11 07:47:03 -0600

    event modals work

---
+++ components/EventModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+function EventModal(props) {
+  const { show, onHide, eventObj } = props;
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{eventObj.venue}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div>{eventObj.date}</div>
+        <div>{eventObj.time}</div>
+        <div>{eventObj.price}</div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+EventModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
+  eventObj: PropTypes.shape({
+    firebaseKey: PropTypes.string.isRequired,
+    artistId: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    venue: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default EventModal;
