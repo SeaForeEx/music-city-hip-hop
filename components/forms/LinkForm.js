3c73ff2a37398556eb88fc2ee8a22fb30a489065
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-03-12 19:21:55 -0500

    cannot change between artist and fan

--- components/forms/LinkForm.js
+++ components/forms/LinkForm.js
@@ -75,7 +75,7 @@ function LinkForm({ obj }) {
         />
       </FloatingLabel>
 
-      <Button type="submit" className="m-2 btn-transparent">{obj.artistId ? 'Edit' : 'Create'} Link</Button>
+      <Button type="submit" className="m-2 btn-transparent">{obj.artistId ? 'Make A Change' : 'Show Us Your Link'}</Button>
     </Form>
   );
 }
--- components/forms/UserForm.js
+++ components/forms/UserForm.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -71,67 +72,74 @@ function UserForm({ obj }) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Change is Good, Change Brings Growth' : 'A New Era has Dawned in Music City'}</h2>
+    <>
+      <Head>
+        <title>{obj.firebaseKey ? 'Change is Good' : 'A New Era has Dawned'}</title>
+      </Head>
+      <Form onSubmit={handleSubmit}>
+        <h2 className="text-white mt-5">{obj.firebaseKey ? 'Change is Good, Change Brings Growth' : 'A New Era has Dawned in Music City'}</h2>
 
-      <FloatingLabel controlId="floatingInput1" label="Name" className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder="What is your Name?"
-          name="name"
-          value={formInput.name}
-          onChange={handleChange}
-          required
-        />
-      </FloatingLabel>
-
-      <FloatingLabel controlId="floatingInput2" label="Bio" className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder="Tell us about yourself"
-          name="bio"
-          value={formInput.bio}
-          onChange={handleChange}
-          required
-        />
-      </FloatingLabel>
-
-      <Form.Group className="mb-3">
-        <Form.Label>Image</Form.Label>
-        <div className="d-flex align-items-center">
+        <FloatingLabel controlId="floatingInput1" label="Name" className="mb-3">
           <Form.Control
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            className="me-3"
+            type="text"
+            placeholder="What is your Name?"
+            name="name"
+            value={formInput.name}
+            onChange={handleChange}
+            required
           />
-          {formInput.image && (
+        </FloatingLabel>
+
+        <FloatingLabel controlId="floatingInput2" label="Bio" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Tell us about yourself"
+            name="bio"
+            value={formInput.bio}
+            onChange={handleChange}
+            required
+          />
+        </FloatingLabel>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Image</Form.Label>
+          <div className="d-flex align-items-center">
+            <Form.Control
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="me-3"
+            />
+            {formInput.image && (
             <img
               src={formInput.image}
               alt="profile"
               style={{ height: '250px', width: '250px', borderRadius: '50%' }}
             />
-          )}
-        </div>
-      </Form.Group>
+            )}
+          </div>
+        </Form.Group>
 
-      <FloatingLabel controlId="floatingSelect" label="AorF">
-        <Form.Select
-          aria-label="Artist-Or-Fan"
-          name="isArtist"
-          onChange={handleChange}
-          className="mb-3"
-          value={formInput.isArtist}
-          required
-        >
-          <option value="">Which Are You?</option>
-          <option value>Artist</option>
-          <option value={false}>Fan</option>
-        </Form.Select>
-      </FloatingLabel>
+        {!obj.firebaseKey && (
+        <FloatingLabel controlId="floatingSelect" label="AorF">
+          <Form.Select
+            aria-label="Artist-Or-Fan"
+            name="isArtist"
+            onChange={handleChange}
+            className="mb-3"
+            value={formInput.isArtist}
+            required
+          >
+            <option value="">Which Are You?</option>
+            <option value>Artist</option>
+            <option value={false}>Fan</option>
+          </Form.Select>
+        </FloatingLabel>
+        )}
 
-      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'}</Button>
-    </Form>
+        <Button type="submit" className="m-2 btn-transparent">{obj.firebaseKey ? 'Make A Change' : 'Join MCHH'}</Button>
+      </Form>
+    </>
   );
 }
 
