115924c2a95d7a1d9b2d9051cf699ebf38fc4207
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-03-27 18:27:46 -0500

    profile image is now required

--- components/NavBar.js
+++ components/NavBar.js
@@ -41,7 +41,7 @@ export default function NavBar() {
             <SearchBar />
             <Link passHref href="/profile">
               <Nav.Link className="navbar-brand">
-                <img src={profilePic.image} alt="img" width="125px" style={{ borderRadius: '50%', marginLeft: '5px' }} id="navbar-profile-image" />
+                <img src={profilePic.image} alt="img" width="125px" style={{ borderRadius: '50%', marginLeft: '5px', objectFit: 'cover' }} id="navbar-profile-image" />
               </Nav.Link>
             </Link>
           </Nav>
