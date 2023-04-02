006c0a6b1f7b81cca2e9ecb4fb8d568531dde0f2
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-04-02 17:06:34 -0500

    all files have comments on them

--- utils/client.js
+++ utils/client.js
@@ -1,22 +1,22 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/storage';
+import firebase from 'firebase/app'; // Imports the firebase/app module, which provides access to the Firebase SDK and core Firebase services.
+import 'firebase/auth'; // Imports the firebase/auth module, which provides authentication services for Firebase.
+import 'firebase/database'; // Imports the firebase/database module, which provides access to the Firebase Realtime Database service.
+import 'firebase/storage'; // Imports the firebase/storage module, which provides access to the Firebase Cloud Storage service.
 
-const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+const clientCredentials = { // Defines an object containing the client's Firebase credentials. These credentials are used to authenticate with the Firebase services.
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, // The API key for the Firebase project.
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, // The authentication domain for the Firebase project.
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID, // The ID of the Firebase project.
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, // The storage bucket for the Firebase project.
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID, // The messaging sender ID for the Firebase project.
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID, // The app ID for the Firebase project.
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL, // The URL for the Firebase Realtime Database service.
 };
 
-if (!firebase.apps.length) {
-  firebase?.initializeApp(clientCredentials);
+if (!firebase.apps.length) { // Checks if the Firebase app has already been initialized.
+  firebase?.initializeApp(clientCredentials); // Initializes the Firebase app with the client's credentials. The optional chaining operator (?.) is used to prevent errors in case the firebase variable is undefined or null.
 }
 
-const storage = firebase.storage();
+const storage = firebase.storage(); // Creates a reference to the Firebase Cloud Storage service.
 
-export { firebase, clientCredentials, storage };
+export { firebase, clientCredentials, storage }; // Exports the firebase, clientCredentials, and storage variables so that they can be used in other modules.
